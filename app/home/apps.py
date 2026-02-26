0e70e53135ab3aa6a9ac939e172201d657a92cc5
tgmjack <tgmjackcroc@gmail.com>
2026-02-26 22:39:16 +0000

    feat(bug): app file fix

--- app/home/apps.py
+++ app/home/apps.py
@@ -4,5 +4,7 @@ from django.apps import AppConfig
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+
     def ready(self):
-        
+        pass
+
