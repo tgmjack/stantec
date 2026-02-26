db3d3ca9fe34508c4b489c4406d8c97382e26e89
tgmjack <tgmjackcroc@gmail.com>
2026-02-26 13:53:31 +0000

    feat(newlogin): fixed loggin in system 2

--- app/stantec/settings.py
+++ app/stantec/settings.py
@@ -86,12 +86,24 @@ WSGI_APPLICATION = 'stantec.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if USE_POSTGRES_DB:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME', 'stantec_db'),
+            'USER': os.getenv('DB_USER', 'stantec_user'),
+            'PASSWORD': os.getenv('DB_PASSWORD', 'stantec_password'),
+            'HOST': os.getenv('DB_HOST', 'postgres'),
+            'PORT': os.getenv('DB_PORT', '5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
